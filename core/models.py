7314883a4c6b87cc26aae770d5b6a55687445794
alin1niroula <alin1niroula@gmail.com>
2023-05-18 14:34:58 +0100

    update

--- core/models.py
+++ core/models.py
@@ -3,14 +3,72 @@ from django.db import models
 # Create your models here.
 
 
-#class Category 
+class Category(models.Model):
+	name = models.CharField(max_length =400 )
+	logo = models.CharField(max_length=300)
+	discriptions = models.CharField(max_length = 800)
+	slug = models.CharField(max_length=300,unique=True)
 
-#class AboutUs
+	def __str__(self):
+		return self.name
 
-#class CourseCategory
 
-#class PopularCourse
+class CourseCategory(models.Model):
+	name = models.CharField(max_length = 300)
+	category = models.ForeignKey(Category,on_delete=models.CASCADE)
+	image = models.ImageField(upload_to='media')
+	slug = models.CharField(max_length=300,unique=True)
 
-#class ExpertInstructors
+	def __str__(self):
+		return self.name
+        
 
-#class OurStudentSays
+class PopularCourse(models.Model):
+	name = models.CharField(max_length = 300)
+	price = models.DecimalField(max_digits=8, decimal_places=2)
+	instructor = models.CharField(max_length=100)
+	duration = models.FloatField()
+	maxstudent = models.IntegerField()
+	category = models.ForeignKey(Category,on_delete=models.CASCADE)
+	coursecategory = models.ForeignKey(CourseCategory,on_delete=models.CASCADE)
+	image = models.ImageField(upload_to='media')
+	slug = models.CharField(max_length=300,unique=True)
+
+	def __str__(self):
+		return self.name
+        
+
+class ExpertInstructors(models.Model):
+	name = models.CharField(max_length=300)
+	image = models.ImageField(upload_to ='media')
+	post = models.CharField(max_length= 300)
+
+	def __str__(self):
+		return self.name
+
+
+class OurStudentSays(models.Model):
+	name = models.CharField(max_length=300)
+	image = models.ImageField(upload_to='media')
+	faculty = models.CharField(max_length = 500)
+	comment = models.TextField()
+
+	def __str__(self):
+		return self.name
+
+
+class Contact(models.Model):
+	name = models.CharField(max_length = 200)
+	subject = models.CharField(max_length=400)
+	message = models.TextField()
+
+	def __str__(self):
+		return self.name
+
+class Gallary(models.Model):
+    image = models.ImageField(upload_to='media')
+
+    def __str__(self):
+        return str(self.image)
+
+        
